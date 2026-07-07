c1fc861b9c8c5da3b92ee87bd0e8f8a9029555fc
oscaresotog <oscar.e.soto.g.ing@gmail.com>
2026-07-07 14:28:58 -0600

    Initialize frontend-backend login integration

---
+++ src/main/java/org/generation/caliope/dto/LoginResponse.java
@@ -0,0 +1,5 @@
+package org.generation.caliope.dto;
+
+
+public record LoginResponse(String token) {
+}
--- src/main/java/org/generation/caliope/repository/UsersRepository.java
+++ src/main/java/org/generation/caliope/repository/UsersRepository.java
@@ -1,7 +1,11 @@
 package org.generation.caliope.repository;
 
+import org.apache.catalina.User;
 import org.generation.caliope.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<Users, Long> {
+   Optional<Users> findByEmail(String email);
 }
